e18db784583ded588bb3aa3080e5f39295ba7b0e
scarioscia <scarios1@jhu.edu>
2022-12-02 09:49:40 -0500

    finish day2

--- R/day2.R
+++ R/day2.R
@@ -2,9 +2,6 @@
 # Score for each round is based on what you choose (1 for Rock, 2 for Paper, and 3 for Scissors) 
 # plus the score for the outcome of the round (0 if you lost, 3 if the round was a draw, and 6 if you won)
 
-# first col is your opponent, second col is you. 
-# opponent: A for Rock, B for Paper, and C for Scissors
-# you: X for Rock, Y for Paper, and Z for Scissors
 
 # load libraries
 library(data.table)
@@ -15,6 +12,11 @@ colnames(input) <- c("opponent", "you")
 
 
 ## PART 1: Your score based on the strategy guide
+
+# first col is your opponent, second col is you. 
+# opponent: A for Rock, B for Paper, and C for Scissors
+# you: X for Rock, Y for Paper, and Z for Scissors
+
 
 # calculate your scores based on shape 
 scores <- input 
@@ -48,4 +50,48 @@ scores_combined <- sum(scores$combined)
 total_scores <- scores_points + scores_combined #13009
 
 
-## PART 2: 
+## PART 2: Score under "correct" strategy guide
+
+# X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win.
+
+new_guide <- input
+new_guide$outcome <- new_guide$you
+new_guide$opponent_outcome <- paste0(new_guide$opponent, new_guide$outcome)
+
+# if outcome is a draw (Y), you need to do the same as your opponent 
+new_guide[new_guide$opponent_outcome == "AY"]$you <- "A"
+new_guide[new_guide$opponent_outcome == "BY"]$you <- "B"
+new_guide[new_guide$opponent_outcome == "CY"]$you <- "C"
+
+# if outcome is you lose, need to lost to your opponent 
+new_guide[new_guide$opponent_outcome == "AX"]$you <- "C" #your opponent did Rock (A), you need to lose (X), so you have to do scissor (C)
+new_guide[new_guide$opponent_outcome == "BX"]$you <- "A" 
+new_guide[new_guide$opponent_outcome == "CX"]$you <- "B" 
+
+# if outcome is you win, need to beat your opponent
+new_guide[new_guide$opponent_outcome == "AZ"]$you <- "B" #your opponent did Rock (A), you need to win (Z), so you have to do paper (B)
+new_guide[new_guide$opponent_outcome == "BZ"]$you <- "C" 
+new_guide[new_guide$opponent_outcome == "CZ"]$you <- "A" 
+
+# calculate your score based on shape 
+new_guide$shape_score <- new_guide$you
+new_guide[new_guide$shape_score == "A"]$shape_score <- 1
+new_guide[new_guide$shape_score == "B"]$shape_score <- 2
+new_guide[new_guide$shape_score == "C"]$shape_score <- 3
+# get score from just the shapes 
+new_guide$shape_score <- new_guide$shape_score %>% as.numeric()
+new_shape_score <- sum(new_guide$shape_score)
+
+# calculate your score based on win, lose, or draw 
+new_guide$win_score <- new_guide$outcome
+new_guide[new_guide$outcome == "X"]$win_score <- 0
+new_guide[new_guide$outcome == "Y"]$win_score <- 3
+new_guide[new_guide$outcome == "Z"]$win_score <- 6
+# get score from the whether you win 
+new_guide$outcome <- new_guide$win_score %>% as.numeric()
+new_outcome_score <- sum(new_guide$outcome)
+
+# get score from shape and outcome 
+new_guide_overall_score <- new_shape_score + new_outcome_score #10398
+
+
